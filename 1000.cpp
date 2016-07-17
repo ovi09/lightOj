c5520e515e7e498c6d6809d35407facccc9d4635
ovi09 <ovi.majumder@yahoo.com>
2016-07-17 15:03:10 +0600

    1000

---
+++ 1000.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+    using namespace std;
+
+    int main()
+    {
+      long t,a,b;
+    int count=1;
+
+      cin>> t;
+      while(t--)
+    {
+     cin>>a>>b;
+     cout<<"Case "<<count<<": "<<a+b<<endl;
+     count++;
+
+    }
+      return 0;
+    }
