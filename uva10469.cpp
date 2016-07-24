0b9018ebed1df804489318932ed2b02e015bd8b2
ovi09 <ovi.majumder@yahoo.com>
2016-07-24 12:34:21 +0600

    uva10071&10469

---
+++ uva10469.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    int a,b;
+
+    while(cin>>a>>b)
+    {
+        int c=a^b;
+        cout<<c<<endl;
+    }
+    return 0;
+}
+
